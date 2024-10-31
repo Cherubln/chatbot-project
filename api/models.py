c5ccf9d949aa3518d1ac60ad334bd32e126ce960
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2024-10-31 10:31:08 +0200

    (models): Add User data model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    bmi = models.FloatField()
+    lifestyle_score = models.IntegerField()
+    insurance_risk = models.IntegerField()
+    diabetes_risk = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.timestamp}"
